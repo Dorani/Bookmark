1cf6153292ae7388ed6b3aed7bee0c9e04f48376
Seif <seif.dorani@gmail.com>
2017-02-07 15:24:08 -0500

    conditional statement if met will display in key val pairs the sitename and url stored in local storage as an obj

--- js/main.js
+++ js/main.js
@@ -13,6 +13,26 @@ function saveBookmark(event){
 
   // local storage test
   localStorage.setItem('test', 'hello world');
+  //test if bookmarks is null
+  if(localStorage.getItem('bookmarks') === null){
+    //init array
+    var bookmarks = [];
+    //add to array
+    bookmarks.push();
+    //set to localStorage
+    //json array and turn it into a string before it gets saved in local storage
+    localStorage.setItem('bookmarks', JSON.stringigy(bookmarks));
+  } else { //if there is something in bookmark
+    //fetch it from local storage
+    //create var that takes in the string that gets parsed back into json
+    var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    //add bookmark to array
+    bookmarks.push(bookmark);
+    //set it back to localStorage
+    localStorage.setItem('bookmarks', JSON.stringigy(bookmarks));
+
+
+  }
 
   //prevent form from submitting
   event.preventDefault();
