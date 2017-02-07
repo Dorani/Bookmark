58cc0dc1b54dd9113f1096588a13bd18f31d7b07
Seif <seif.dorani@gmail.com>
2017-02-07 13:56:08 -0500

    when we submit to local storage, we will save it as an array of obj, we want an obj ready to create to local storage

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(event){
   //Get form values
-  var siteName = document.getElementById('siteName');
+  var siteName = document.getElementById('siteName').value;
+  var siteUrl = document.getElementById('siteUrl').value;
+
+  var bookmark = {
+    name: siteName,
+    url: siteUrl
+  }
+
+  // local storage test
+  l
 
   //prevent form from submitting
   event.preventDefault();
